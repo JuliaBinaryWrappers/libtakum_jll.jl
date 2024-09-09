82cb6aeb598329952c1e328157b5635b52650b1c
jlbuild <juliabuildbot@gmail.com>
2024-09-09 22:02:24 +0000

    libtakum_jll build 0.3.0+0

--- src/wrappers/i686-linux-gnu.jl
+++ src/wrappers/i686-linux-gnu.jl
@@ -2,7 +2,7 @@
 export libtakum
 
 JLLWrappers.@generate_wrapper_header("libtakum")
-JLLWrappers.@declare_library_product(libtakum, "libtakum.so.0.2")
+JLLWrappers.@declare_library_product(libtakum, "libtakum.so.0.3")
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
