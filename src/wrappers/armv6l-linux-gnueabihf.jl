e721c445f37eca7049c882fd498621e091b1d59b
jlbuild <juliabuildbot@gmail.com>
2024-10-01 17:53:59 +0000

    libtakum_jll build 0.4.0+0

--- src/wrappers/armv6l-linux-gnueabihf.jl
+++ src/wrappers/armv6l-linux-gnueabihf.jl
@@ -2,7 +2,7 @@
 export libtakum
 
 JLLWrappers.@generate_wrapper_header("libtakum")
-JLLWrappers.@declare_library_product(libtakum, "libtakum.so.0.3")
+JLLWrappers.@declare_library_product(libtakum, "libtakum.so.0.4")
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
